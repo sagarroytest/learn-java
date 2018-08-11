6566b6370f32dfae99bfcdb3798b0d21f44f2825
Sagar roy <roysagar1977@gmail.com>
2018-08-11 18:51:44 -0400

    Data changed

--- src/com/javahomework/ForLoopMultiplication.java
+++ src/com/javahomework/ForLoopMultiplication.java
@@ -3,7 +3,7 @@ package com.javahomework;
 public class ForLoopMultiplication {
 
 	public static void main(String[] args) {
-		int a=8;
+		int a=10;
 		for(int b=1; b<=10; b++) {
 			int c=a*b;
 			System.out.println(a+"x"+b+"="+c);
