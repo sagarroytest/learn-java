cabba2375e71b2762be23128836f994c5bcbca58
Sagar roy <roysagar1977@gmail.com>
2018-08-18 18:40:04 -0400

    New File Added

---
+++ src/com/javalecture/MultiplicationAndDivide.java
@@ -0,0 +1,24 @@
+package com.javalecture;
+
+public class MultiplicationAndDivide {
+
+	public static void main(String[] args) {
+		
+		int num1= 5, num2 = 10;
+		int result = num1*num2;
+		
+		System.out.println("The result: * result");
+				
+
+	}
+	{
+	
+	int num1 = 10;
+	int num2 = 5;
+	int res = num1%num2;
+	
+	                     {
+	System.out.println("The result: % result");
+}
+}
+}
